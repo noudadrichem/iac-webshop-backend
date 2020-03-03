4a7890a413467a4833e1e7aa6da1d736c44d9475
noudadrichem <info@noudadrichem.com>
2020-03-03 14:12:23 +0100

    init

---
+++ src/main/java/hu/iac/webshop/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hu.iac.webshop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebshopApplication.class);
+	}
+
+}
