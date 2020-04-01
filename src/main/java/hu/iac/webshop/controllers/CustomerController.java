884fa0bb2a1ec710b6c9791edec77f3ee39d33b1
stefanVanEchtelt <stefan.vanechtelt@student.hu.nl>
2020-04-01 12:11:23 +0200

    customer create queue

--- src/main/java/hu/iac/webshop/controllers/CustomerController.java
+++ src/main/java/hu/iac/webshop/controllers/CustomerController.java
@@ -1,19 +1,31 @@
 package hu.iac.webshop.controllers;
 
 import hu.iac.webshop.domain.Customer;
-import hu.iac.webshop.dto.product.CategoryRequest;
 import hu.iac.webshop.dto.product.CustomerRequest;
 import hu.iac.webshop.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConversionException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.jms.Queue;
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 public class CustomerController {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private Queue queue;
+
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
@@ -26,14 +38,16 @@ public class CustomerController {
     }
 
     @PostMapping("/customer")
-    public Customer create(@Valid @RequestBody CustomerRequest customerRequest){
-        Customer customer = new Customer(
-            customerRequest.getName(),
-            customerRequest.getPhone(),
-            customerRequest.getEmail()
-        );
+    public Map<String, Object> create(@Valid @RequestBody CustomerRequest customerRequest) throws MessageConversionException {
+        Map<String, Object> customerMap = new HashMap<>();
 
-        return this.customerService.create(customer);
+        customerMap.put("name", customerRequest.getName());
+        customerMap.put("phone", customerRequest.getPhone());
+        customerMap.put("email", customerRequest.getEmail());
+
+        jmsTemplate.convertAndSend(queue, customerMap);
+
+        return customerMap;
     }
 
     @PutMapping("/category/{id}")
